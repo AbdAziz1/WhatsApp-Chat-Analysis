5847895b2e26e371113f5942f2a3fa1d4a0451e8
Abdul Aziz <118743899+AbdAziz1@users.noreply.github.com>
2025-02-14 13:55:51 +0500

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import seaborn as sns
 
 st.sidebar.title('WhatsApp Chat Analyzer')
 
-uploaded_file = st.sidebar.file_uploader('Choose a file')
+uploaded_file = st.sidebar.file_uploader('Upload WhatsApp chat text file')
 if uploaded_file is not None:
     bytes_data = uploaded_file.getvalue()
     data = bytes_data.decode("utf-8")
